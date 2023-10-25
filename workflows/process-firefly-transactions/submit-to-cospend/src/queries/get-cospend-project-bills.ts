f7c99a07657afb18cd881773063233d0b482700c
Nikita Volodin <volodin.n@gmail.com>
2023-10-25 11:18:13 -0400

    feat: implement GetCospendProjectBills query

---
+++ workflows/process-firefly-transactions/submit-to-cospend/src/queries/get-cospend-project-bills.ts
@@ -0,0 +1,49 @@
+import { Schema as S } from "@effect/schema";
+import { ParseError } from "@effect/schema/ParseResult";
+import { Data, Effect as T, Request, RequestResolver } from "effect";
+import { pipe } from "effect/Function";
+import {
+  CospendProjectBillsS,
+  CospendProjectBillsTo,
+  ProjectId,
+} from "../model/cospend.js";
+import { CospendApiService } from "./axios-instances.js";
+import { NetworkError } from "./errors.js";
+
+export class GetCospendProjectBillsError extends Data.TaggedError(
+  "GetCospendProjectBillsError",
+)<{ error: NetworkError | ParseError }> {}
+
+export interface GetCospendProjectBills
+  extends Request.Request<GetCospendProjectBillsError, CospendProjectBillsTo> {
+  readonly _tag: "GetCospendProjectBills";
+  readonly project: ProjectId;
+}
+
+export const GetCospendProjectBills = Request.tagged<GetCospendProjectBills>(
+  "GetCospendProjectBills",
+);
+
+export const GetCospendProjectBillsResolver = pipe(
+  ({ project }: GetCospendProjectBills) =>
+    pipe(
+      CospendApiService,
+      T.flatMap(({ client }) =>
+        client.request({
+          method: "get",
+          url: `/api-priv/projects/${project}/bills`,
+        }),
+      ),
+      T.map((_) => _.data),
+      T.flatMap((_) => S.parse(CospendProjectBillsS)(_, { errors: "all" })),
+      T.mapError((error) => new GetCospendProjectBillsError({ error })),
+    ),
+  RequestResolver.fromFunctionEffect,
+  RequestResolver.contextFromServices(CospendApiService),
+);
+
+export const getCospendProjectBills = (project: ProjectId) =>
+  T.request(
+    GetCospendProjectBills({ project }),
+    GetCospendProjectBillsResolver,
+  );
