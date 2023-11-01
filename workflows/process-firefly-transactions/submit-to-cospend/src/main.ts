ea243d34293e620293750352b4d0df7558c29ebe
Nikita Volodin <volodin.n@gmail.com>
2023-10-31 21:08:11 -0400

    refactor: print all errors and use platform runtime

--- workflows/process-firefly-transactions/submit-to-cospend/src/main.ts
+++ workflows/process-firefly-transactions/submit-to-cospend/src/main.ts
@@ -1,5 +1,4 @@
-import * as os from "node:os";
-import Axios from "axios";
+import * as core from "@actions/core";
 import * as S from "@effect/schema/Schema";
 import { InputEnvVars } from "./model/program-inputs.js";
 import { formatErrors } from "@effect/schema/TreeFormatter";
@@ -10,44 +9,34 @@ import {
   FireflyApiServiceLive,
 } from "./queries/axios-instances.js";
 import { program } from "./program.js";
-import { Layer } from "effect";
+import { Cause, Layer } from "effect";
+import { Runtime } from "@effect/platform-node";
+import os from "node:os";
 
-const main = T.gen(function* (_) {
-  const conf = yield* _(
-    pipe(
-      T.sync(() => process.env),
-      T.flatMap((_) => S.parse(InputEnvVars)(_, { errors: "all" })),
-      T.catchTag("ParseError", ({ errors }) =>
-        T.fail({
-          title: "The environment is missing required variables",
-          details: formatErrors(errors),
-        }),
-      ),
+pipe(
+  T.sync(() => process.env),
+  T.flatMap((_) => S.parse(InputEnvVars)(_, { errors: "all" })),
+  T.catchTag("ParseError", ({ errors }) =>
+    T.fail(
+      "The environment is missing required variables" +
+        os.EOL +
+        formatErrors(errors),
     ),
-  );
+  ),
+  T.flatMap((conf) => {
+    const layer = pipe(
+      CospendApiServiceLive(conf.nc_base_url, conf.nc_user, conf.nc_password),
+      Layer.merge(
+        FireflyApiServiceLive(conf.ff3_base_url, conf.input.info.pat),
+      ),
+    );
 
-  const layer = pipe(
-    CospendApiServiceLive(conf.nc_base_url, conf.nc_user, conf.nc_password),
-    Layer.merge(FireflyApiServiceLive(conf.ff3_base_url, conf.input.info.pat)),
-  );
-
-  return yield* _(T.provide(program(conf), layer));
-});
-
-function handleError(err: unknown): void {
-  const msg = err instanceof Error ? err.toString() : err;
-
-  if (Axios.isAxiosError(err)) {
-    console.log(err.toJSON());
-  }
-
-  // github action way of setting an error. see:
-  // https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-error-message
-  const cmd = `::error::${msg}${os.EOL}`;
-
-  process.exitCode = 1;
-  process.stdout.write(cmd);
-}
-
-process.on("unhandledRejection", handleError);
-T.runPromise(main).catch(handleError);
+    return T.provide(program(conf), layer);
+  }),
+  T.tapErrorCause((cause) =>
+    T.sync(() => {
+      core.error(Cause.pretty(cause));
+    }),
+  ),
+  Runtime.runMain,
+);
